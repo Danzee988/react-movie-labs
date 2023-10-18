077c1b3158c3aa7086361745ec43ee18d62f11c5
Daniel-Wolski <20098120@mail.wit.ie>
2023-10-18 19:13:08 +0100

    Refactor - useMovie custom hook

--- movies/src/pages/movieReviewPage.js
+++ movies/src/pages/movieReviewPage.js
@@ -3,6 +3,7 @@ import { useLocation } from "react-router-dom";
 import PageTemplate from "../components/templateMoviePage";
 import MovieReview from "../components/movieReview";
 
+
 const MovieReviewPage = (props) => {
   let location = useLocation();
   const {movie, review} = location.state;
